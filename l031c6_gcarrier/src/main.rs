8d8a04cab193d28e4785ccf20485d5b9043c8972
Roman Isaikin <romix.lab@gmail.com>
2020-10-12 19:03:23 +0200

    Misc stuff.

--- l031c6_gcarrier/src/main.rs
+++ l031c6_gcarrier/src/main.rs
@@ -2,20 +2,23 @@
 #![no_std]
 #![feature(alloc_error_handler)]
 
+mod tasks;
+mod config;
+mod power_block;
+
 use core::fmt::Write;
 use jlink_rtt::NonBlockingOutput;
-#[allow(unused_imports)]
-use panic_ramlog;
+//#[allow(unused_imports)]
+//use panic_ramlog;
 
 use alloc_cortex_m::CortexMHeap;
 #[global_allocator]
 static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
 extern crate alloc;
-use alloc::vec::Vec;
 use alloc::boxed::Box;
 
 use rtic::app;
-use rtic::Monotonic;
+//use rtic::Monotonic;
 
 use stm32l0xx_hal as hal;
 use hal::prelude::*;
@@ -23,20 +26,12 @@ use hal::gpio::{Output, PushPull};
 use hal::gpio::gpioc::PC14;
 use hal::rcc::MSIRange;
 
-use mcu_helper::tim_cyccnt::{TimCyccnt, U32Ext};
+use mcu_helper::tim_cyccnt::{U32Ext};
+use power_helper::power_block::{PowerBlock, PowerBlockType, DummyInputPin, PowerBlockControl};
+use crate::power_block::PowerBlockId;
 
 use mcp25625::{MCP25625, MCP25625Config};
-
-use hal::gpio::gpioa::{PA4, PA5, PA6, PA7};
-use hal::gpio::Analog;
-//use hal::gpio::PushPull;
-type Mcp25625Sck = PA5<Analog>;
-type Mcp25625Miso = PA6<Analog>;
-type Mcp25625Mosi = PA7<Analog>;
-type Mcp25625Cs = PA4<Output<PushPull>>;
-type Mcp25625Instance = mcp25625::MCP25625<hal::spi::Spi<hal::pac::SPI1, (Mcp25625Sck, Mcp25625Miso, Mcp25625Mosi)>, Mcp25625Cs>;
-
-use power_helper::power_block::{PowerBlock, PowerBlockType, DummyInputPin, PowerBlockControl};
+use bq769x0::BQ769x0;
 
 #[app(device = stm32l0xx_hal::pac, peripherals = true, monotonic = mcu_helper::tim_cyccnt::TimCyccnt)]
 const APP: () = {
@@ -44,7 +39,11 @@ const APP: () = {
         clocks: hal::rcc::Clocks,
         status_led: PC14<Output<PushPull>>,
         rtt: NonBlockingOutput,
-        mcp25625: Mcp25625Instance
+        mcp25625: config::Mcp25625Instance,
+        i2c: config::InternalI2c,
+        bq76920: BQ769x0,
+        tms_sop: config::TmsSopPin,
+        power_blocks: config::PowerBlocksMap
     }
 
     #[init(
@@ -75,85 +74,90 @@ const APP: () = {
 
         // Power switches and DC-DCs
         use PowerBlockType::*;
+        use PowerBlockId::*;
+        // Be careful with the map size!
+        let mut power_blocks = config::PowerBlocksMap::new();
         // BMS Low Iq DC-DC
         let dcdc_5v0_bms_en = gpioc.pc13.into_push_pull_output();
-        let pb_5v0_bms: PowerBlock<_, DummyInputPin> = PowerBlock::new(
-            DcDc, dcdc_5v0_bms_en, None, "5v0_bms"
+        let mut pb_5v0_bms: PowerBlock<_, DummyInputPin> = PowerBlock::new(
+            DcDc, dcdc_5v0_bms_en, None
         );
+        pb_5v0_bms.enable(); // enable right away
+        let _ = power_blocks.insert(DcDc5V0Bms, Box::new(pb_5v0_bms)); // .expect("I0");
         // CAN bus transceivers switch U16
         let ps_5v0_syscan_en = gpioa.pa3.into_push_pull_output();
         let ps_5v0_syscan_pgood = gpiob.pb3.into_pull_up_input();
         let pb_5v0_syscan = PowerBlock::new(
-            Switch, ps_5v0_syscan_en, Some(ps_5v0_syscan_pgood), "5v0_syscan"
+            Switch, ps_5v0_syscan_en, Some(ps_5v0_syscan_pgood)
         );
+        let _ = power_blocks.insert(Switch5V0Syscan, Box::new(pb_5v0_syscan));
         // External connector switch U15
         let ps_5v0_m12_en = gpiob.pb11.into_push_pull_output();
-        let ps_5v0_m12_pgood = gpiob.pb8.into_pull_up_input();
-        let pb_5v0_m12 = PowerBlock::new(
-            Switch, ps_5v0_m12_en, Some(ps_5v0_m12_pgood), "5v0_m12"
+        //let ps_5v0_m12_pgood = gpiob.pb8.into_pull_up_input(); // trace reworked
+        let mut tms_sop = gpiob.pb8.into_open_drain_output(); // TDO net
+        tms_sop.set_high().ok(); // high - normal state, low - into jtag mode during power on
+        let pb_5v0_m12: PowerBlock<_, DummyInputPin> = PowerBlock::new(
+            Switch, ps_5v0_m12_en, None
         );
+        let _ = power_blocks.insert(Switch5V0M12, Box::new(pb_5v0_m12));
         // Flex to radars switch U17
         let ps_5v0_flex_en = gpiob.pb9.into_push_pull_output();
         let ps_5v0_flex_pgood = gpiob.pb4.into_pull_up_input();
         let pb_5v0_flex = PowerBlock::new(
-            Switch, ps_5v0_flex_en, Some(ps_5v0_flex_pgood), "5v0_flex"
+            Switch, ps_5v0_flex_en, Some(ps_5v0_flex_pgood)
         );
+        let _ = power_blocks.insert(Switch5V0Flex, Box::new(pb_5v0_flex));
         // 5V0 DC-DC U7
         let dcdc_5v0_hc_en = gpiob.pb0.into_push_pull_output();
         let dcdc_5v0_hc_pgood = gpiob.pb2.into_pull_up_input();
         let pb_5v0_hc = PowerBlock::new(
-            DcDc, dcdc_5v0_hc_en, Some(dcdc_5v0_hc_pgood), "5v0_hc"
+            DcDc, dcdc_5v0_hc_en, Some(dcdc_5v0_hc_pgood)
         );
+        let _ = power_blocks.insert(DcDc5V0Hc, Box::new(pb_5v0_hc));
         // 3V3 DC-DC U9
         let dcdc_3v3_hc_en = gpioa.pa8.into_push_pull_output();
         let dcdc_3v3_hc_pgood = gpiob.pb15.into_pull_up_input();
         let pb_3v3_hc = PowerBlock::new(
-            DcDc, dcdc_3v3_hc_en, Some(dcdc_3v3_hc_pgood), "3v3_hc"
+            DcDc, dcdc_3v3_hc_en, Some(dcdc_3v3_hc_pgood)
         );
+        let _ = power_blocks.insert(DcDc3V3Hc, Box::new(pb_3v3_hc));
         // HC switch U13 to DC-DC U9, LC switch U14 to BMS Low Iq DC-DC, OR-ed fault
         let ps_3v3_uwblc_en = gpiob.pb12.into_push_pull_output();
         let ps_3v3_uwbhc_en = gpiob.pb6.into_push_pull_output();
         let ps_3v3_uwb_pgood = gpiob.pb7.into_pull_up_input();
         let pb_3v3_uwblc = PowerBlock::new(
-            Switch, ps_3v3_uwblc_en, Some(ps_3v3_uwb_pgood), "3v3_uwblc"
+            Switch, ps_3v3_uwblc_en, Some(ps_3v3_uwb_pgood)
         );
         let pb_3v3_uwbhc: PowerBlock<_, DummyInputPin> = PowerBlock::new(
-            Switch, ps_3v3_uwbhc_en, None, "3v3_uwbhc"
+            Switch, ps_3v3_uwbhc_en, None
         );
+        let _ = power_blocks.insert(Switch3V3UWBLc, Box::new(pb_3v3_uwblc));
+        let _ = power_blocks.insert(Switch3V3UWBHc, Box::new(pb_3v3_uwbhc));
         // TMS switch U11
         let ps_3v3_tms_en = gpiob.pb5.into_push_pull_output();
         let ps_3v3_tms_pgood = gpioc.pc15.into_pull_up_input();
         let pb_3v3_tms = PowerBlock::new(
-            Switch, ps_3v3_tms_en, Some(ps_3v3_tms_pgood), "3v3_tms"
+            Switch, ps_3v3_tms_en, Some(ps_3v3_tms_pgood)
         );
+        let _ = power_blocks.insert(Switch3V3Tms, Box::new(pb_3v3_tms));
         // IMX DC-DC U6
         let dcdc_3v8_imx_en = gpiob.pb14.into_push_pull_output();
         let dcdc_3v8_imx_flt = gpiob.pb1.into_pull_up_input();
         let pb_3v8_imx = PowerBlock::new(
-            DcDc, dcdc_3v8_imx_en, Some(dcdc_3v8_imx_flt), "3v8_imx"
+            DcDc, dcdc_3v8_imx_en, Some(dcdc_3v8_imx_flt)
         );
+        let _ = power_blocks.insert(DcDc3V8Imx, Box::new(pb_3v8_imx));
         // USB switch U12
         let ps_5v0_usb_en = gpiob.pb13.into_push_pull_output();
         let ps_5v0_usb_pgood = gpiob.pb10.into_pull_up_input();
-        let pb_3v8_usb = PowerBlock::new(
-            Switch, ps_5v0_usb_en, Some(ps_5v0_usb_pgood), "5v0_usb"
+        let pb_5v0_usb = PowerBlock::new(
+            Switch, ps_5v0_usb_en, Some(ps_5v0_usb_pgood)
         );
+        let _ = power_blocks.insert(Switch5V0Usb, Box::new(pb_5v0_usb));
 
-        let mut power_blocks: Vec<Box<dyn PowerBlockControl>> = Vec::new();
-        power_blocks.push(Box::new(pb_5v0_bms));
-        power_blocks.push(Box::new(pb_5v0_syscan));
-        power_blocks.push(Box::new(pb_5v0_m12));
-        power_blocks.push(Box::new(pb_5v0_flex));
-        power_blocks.push(Box::new(pb_5v0_hc));
-        power_blocks.push(Box::new(pb_3v3_hc));
-        power_blocks.push(Box::new(pb_3v3_uwblc));
-        power_blocks.push(Box::new(pb_3v3_uwbhc));
-        power_blocks.push(Box::new(pb_3v3_tms));
-        power_blocks.push(Box::new(pb_3v8_imx));
-        power_blocks.push(Box::new(pb_3v8_usb));
+        power_blocks.get_mut(&PowerBlockId::DcDc3V3Hc).expect("I1").enable();
+        power_blocks.get_mut(&PowerBlockId::Switch3V3Tms).expect("I1").disable();
 
-        power_blocks[0].enable();
-        power_blocks[1].enable();
 
         // SPI<->CANBus MCP25625T-E/ML
         let mcp_freq = 1.mhz();
@@ -215,16 +219,18 @@ const APP: () = {
             ov_threshold: MilliVolts(4150)
         };
         match bq76920.init(&mut i2c, &bq769x0_config) {
-            Ok(actual) => {
-                let _ = writeln!(rtt, "bq769x0 init ok");
-                let _ = writeln!(rtt, "adc gain:{}uV/LSB offset:{}mV", bq76920.adc_gain(), bq76920.adc_offset());
-                let _ = writeln!(rtt, "SCD: {}, OCD: {}, range: {:?}", actual.scd_threshold, actual.ocd_threshold, actual.ocdscd_range_used);
-                let _ = writeln!(rtt, "UV: {}, OV: {}", actual.uv_threshold, actual.ov_threshold);
+            Ok(_actual) => {
+                let _ = writeln!(rtt, "afe init ok");
+                //let _ = writeln!(rtt, "adc gain:{}uV/LSB offset:{}mV", bq76920.adc_gain(), bq76920.adc_offset());
+                //let _ = writeln!(rtt, "SCD: {}, OCD: {}, range: {:?}", actual.scd_threshold, actual.ocd_threshold, actual.ocdscd_range_used);
+                //let _ = writeln!(rtt, "UV: {}, OV: {}", actual.uv_threshold, actual.ov_threshold);
             }
             Err(e) => {
-                let _ = writeln!(rtt, "bq769x0 init err: {:?}", e);
+                let _ = writeln!(rtt, "afe init err:{:?}", e);
             }
         }
+        let r = bq76920.discharge(&mut i2c, true);
+        let _ = writeln!(rtt, "DE: {:?}", r);
 
         cx.spawn.blinker().ok();
 
@@ -233,6 +239,10 @@ const APP: () = {
             status_led,
             rtt,
             mcp25625,
+            i2c,
+            bq76920,
+            tms_sop,
+            power_blocks,
         }
     }
 
@@ -252,63 +262,54 @@ const APP: () = {
         let schedule_at = cx.scheduled + 2_000_000.cycles();
         cx.schedule.blinker(schedule_at).ok();
 
-        let rtt = cx.resources.rtt;
+        //let rtt = cx.resources.rtt;
 
-        use mcp25625::{McpReceiveBuffer, CanAddress, McpPriority};
-        let mcp25625 = cx.resources.mcp25625;
-        let intf = mcp25625.interrupt_flags();
-        writeln!(rtt, "{:?}", intf).ok();
-        let errf = mcp25625.error_flags();
-        writeln!(rtt, "{:?}", errf).ok();
-
-        if intf.rx0if_is_set() {
-            let message = mcp25625.receive(McpReceiveBuffer::Buffer0);
-            writeln!(rtt, "rx0:{:?}", message).ok();
-        }
-        if intf.rx1if_is_set() {
-            let message = mcp25625.receive(McpReceiveBuffer::Buffer1);
-            writeln!(rtt, "rx1:{:?}", message).ok();
-        }
-        let tec = mcp25625.tec();
-        let rec = mcp25625.rec();
-        writeln!(rtt, "tec:{}, rec:{}", tec, rec).ok();
-
-        //mcp25625.reset_interrupt_flags(intf.bits);
-        //mcp25625.reset_error_flags();
-
-        let mut empty_tx_bufs = 0u8;
-        if intf.tx0if_is_set() {
-            empty_tx_bufs = empty_tx_bufs + 1
-        }
-        if intf.tx1if_is_set() {
-            empty_tx_bufs = empty_tx_bufs + 1
-        }
-        if intf.tx2if_is_set() {
-            empty_tx_bufs = empty_tx_bufs + 1
-        }
-        if empty_tx_bufs > 0 {
-            writeln!(rtt, "{} tx buffers free", empty_tx_bufs).ok();
-        }
-
-
-        let r = mcp25625.send(CanAddress::extended(0x1769A5F3), &[0xaa, 0xbb, 0xcc, 0xdd], McpPriority::Low);
-        writeln!(rtt, "tx: {:?}", r).ok();
-        writeln!(rtt, "\n\n\n").ok();
+        // use mcp25625::{McpReceiveBuffer, CanAddress, McpPriority};
+        // let mcp25625 = cx.resources.mcp25625;
+        // let intf = mcp25625.interrupt_flags();
+        // writeln!(rtt, "{:?}", intf).ok();
+        // let errf = mcp25625.error_flags();
+        // writeln!(rtt, "{:?}", errf).ok();
+        //
+        // if intf.rx0if_is_set() {
+        //     let message = mcp25625.receive(McpReceiveBuffer::Buffer0);
+        //     writeln!(rtt, "rx0:{:?}", message).ok();
+        // }
+        // if intf.rx1if_is_set() {
+        //     let message = mcp25625.receive(McpReceiveBuffer::Buffer1);
+        //     writeln!(rtt, "rx1:{:?}", message).ok();
+        // }
+        // let tec = mcp25625.tec();
+        // let rec = mcp25625.rec();
+        // writeln!(rtt, "tec:{}, rec:{}", tec, rec).ok();
+        //
+        // //mcp25625.reset_interrupt_flags(intf.bits);
+        // //mcp25625.reset_error_flags();
+        //
+        // let mut empty_tx_bufs = 0u8;
+        // if intf.tx0if_is_set() {
+        //     empty_tx_bufs = empty_tx_bufs + 1
+        // }
+        // if intf.tx1if_is_set() {
+        //     empty_tx_bufs = empty_tx_bufs + 1
+        // }
+        // if intf.tx2if_is_set() {
+        //     empty_tx_bufs = empty_tx_bufs + 1
+        // }
+        // if empty_tx_bufs > 0 {
+        //     writeln!(rtt, "{} tx buffers free", empty_tx_bufs).ok();
+        // }
+        //
+        //
+        // let r = mcp25625.send(CanAddress::extended(0x1769A5F3), &[0xaa, 0xbb, 0xcc, 0xdd], McpPriority::Low);
+        // writeln!(rtt, "tx: {:?}", r).ok();
     }
 
     #[idle(
-        resources = [rtt]
+        resources = [i2c, bq76920, rtt, power_blocks, tms_sop]
     )]
-    fn idle(mut cx: idle::Context) -> ! {
-        use core::sync::atomic;
-        loop {
-            let instant = TimCyccnt::now();
-            cx.resources.rtt.lock(|rtt| {
-                writeln!(rtt, "idle {:?}", instant).ok();
-            });
-            cortex_m::asm::delay(2_000_000);
-            atomic::compiler_fence(atomic::Ordering::SeqCst);
-        }
+    fn idle(cx: idle::Context) -> ! {
+        tasks::idle::idle(cx);
     }
 
     extern "C" {
@@ -319,11 +320,18 @@ const APP: () = {
 use cortex_m_rt::exception;
 #[exception]
 fn HardFault(ef: &cortex_m_rt::ExceptionFrame) -> ! {
-    panic!("HardFault: {:#?}", ef);
+    panic!("HF: {:#?}", ef);
 }
 
 use core::alloc::Layout;
 #[alloc_error_handler]
 fn oom(_: Layout) -> ! {
     panic!("OOM");
+}
+
+use core::panic::PanicInfo;
+#[inline(never)]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    cortex_m::peripheral::SCB::sys_reset(); // -> !
 }
